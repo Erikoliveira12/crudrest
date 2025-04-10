99133a14cd188b681e4e81925c369c06534f3e37
Erik oliveira <Darkerik221@gmail.com>
2025-04-09 21:50:58 -0300

    Versão inicial

---
+++ src/main/java/com/belval/crudrest/controller/ProdutoController.java
@@ -0,0 +1,46 @@
+package com.belval.crudrest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.belval.crudrest.model.Produto;
+import com.belval.crudrest.repository.ProdutoRepository;
+
+@RestController
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository repository;
+	
+	@GetMapping("/produtos")
+	public ResponseEntity<Iterable<Produto>> obterProdutos() {
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(repository.findAll());
+	}
+	
+	
+	//curl POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
+	
+	@PostMapping("/produtos")
+	public ResponseEntity<Produto> criarProduto(
+			@RequestBody Produto produto) {
+		
+		//produto.setId(proxId++);
+		
+		System.out.println("Produto criado ... " + produto.toString());
+		repository.save(produto);
+		
+		
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(produto);
+		
+	}
+
+}
